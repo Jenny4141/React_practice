4ade8f7951ebbca18dc86315c91a31cc3fb4b451
Jenny <jenny0509419@gmail.com>
2025-08-14 01:16:39 +0800

    修改next內建檔案路徑、新增todolist頁面

---
+++ app/todolist/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function ToDoListPage() {
+  return (
+    <div className="container">
+      <h1 className="text-base">TO-DO-LIST</h1>
+    </div>
+  );
+}
