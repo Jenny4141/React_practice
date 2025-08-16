64ca15a7a6f70e92578433a97ea978f09f79906d
Jenny <jenny0509419@gmail.com>
2025-08-16 17:36:47 +0800

    新增待辦事項選項

--- app/todolist/page.js
+++ app/todolist/page.js
@@ -6,15 +6,22 @@ import { IoTrashOutline } from "react-icons/io5";
 
 export default function ToDoListPage() {
   // 狀態處理
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([
+    "React專案練習",
+    "作品集整理",
+    "企劃書撰寫",
+  ]);
   const [newTask, setNewTask] = useState("");
   // 事件處理函數
   const handleInputChange = (event) => {
     setNewTask(event.target.value);
   };
+  const addTask = () => {};
+  const deleteTask = (index) => {};
+
   return (
-    <div className="container flex flex-col justify-center items-center mx-auto max-w-[400px] py-6 border rounded">
-      <h1 className="text-lg font-bold mb-3">TO-DO-LIST</h1>
+    <div className="container flex flex-col justify-center items-center mx-auto max-w-[400px] py-6 border rounded gap-3">
+      <h1 className="text-lg font-bold">TO-DO-LIST</h1>
       <div className="input-group flex items-center gap-1">
         <input
           type="text"
@@ -23,10 +30,20 @@ export default function ToDoListPage() {
           value={newTask}
           onChange={handleInputChange}
         />
-        <button className="w-8 h-8 bg-red-300 flex items-center justify-center rounded">
+        <button className="w-8 h-8 bg-red-300 flex justify-center items-center rounded">
           <IoMdAdd />
         </button>
       </div>
+      <ol>
+        {tasks.map((task, idx) => (
+          <li key={idx} className="flex gap-1">
+            <span className="text-base">{task}</span>
+            <button className="w-5 h-5 bg-gray-400 flex justify-center items-center rounded">
+              <IoTrashOutline />
+            </button>
+          </li>
+        ))}
+      </ol>
     </div>
   );
 }
